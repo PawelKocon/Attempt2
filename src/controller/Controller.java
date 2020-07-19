97381bb395fbdb0f3e80799175a5e028fcca0acc
PawelKocon <42476065+PawelKocon@users.noreply.github.com>
2020-07-19 13:51:58 +0200

    test commit

---
+++ src/controller/Controller.java
@@ -0,0 +1,22 @@
+package controller;
+
+
+import java.util.Vector;
+
+public class Controller {
+    public static void main(String[] args) {
+        //przygotowanie danych do przekazania do metod klas.
+
+            Vector<Double> tab=new Vector<Double>();
+            /*
+            * uzywamy kontenerów zamiast tablic, kontener budowa kontener
+            * to klasa do przechowywania iluś elementów
+            *
+            *
+            * */
+            for (int i = 0; i< args.length-1; i++)
+            {
+                    tab.add(Double.parseDouble())
+            }
+    }
+}
